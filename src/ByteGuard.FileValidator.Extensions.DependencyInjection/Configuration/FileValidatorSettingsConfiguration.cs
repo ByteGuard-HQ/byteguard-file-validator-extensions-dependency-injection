0a66768dfd1af05774320315d34ce484b473f329
ChristianHaase <christian.haase.93@gmail.com>
2025-11-29 11:00:36 +0100

    feat: preparation for initial release

--- src/ByteGuard.FileValidator.Extensions.DependencyInjection/Configuration/FileValidatorSettingsConfiguration.cs
+++ src/ByteGuard.FileValidator.Extensions.DependencyInjection/Configuration/FileValidatorSettingsConfiguration.cs
@@ -1,6 +1,47 @@
 namespace ByteGuard.FileValidator.Extensions.DependencyInjection.Configuration;
 
+/// <summary>
+/// Appsettings specific configuration for the File Validator.
+/// </summary>
 public class FileValidatorSettingsConfiguration
 {
+    /// <summary>
+    /// Supported file types.
+    /// </summary>
+    /// <remarks>
+    /// Setup which file types are supported by the file validator.
+    /// File types should be specified with their leading dot, e.g. <c>.jpg</c>, <c>.png</c>, <c>.pdf</c>, etc.
+    /// Currently supported file types can be found in <see cref="FileExtensions"/>.
+    /// </remarks>
+    public List<string> SupportedFileTypes { get; set; } = new List<string>();
 
+    /// <summary>
+    /// Maximum file size limit in bytes.
+    /// </summary>
+    /// <remarks>
+    /// Defines the file size limit of files. See <see cref="ByteSize"/> for conversion help.
+    /// </remarks>
+    public long FileSizeLimit { get; set; } = -1;
+
+    /// <summary>
+    /// Maximum file size limit in unit string representation (e.g. "25MB", "2 GB", etc.).
+    /// </summary>
+    /// <remarks>
+    /// Will be ignored if <see cref="FileSizeLimit"/> is defined.
+    /// Spacing (<c>"25 MB"</c> vs. <c>"25MB"</c>) is irrelevant.
+    /// <para>Supported string representation are:
+    /// <ul>
+    /// <li><c>B</c>: Bytes</li>
+    /// <li><c>KB</c>: Kilobytes</li>
+    /// <li><c>MB</c>: Megabytes</li>
+    /// <li><c>GB</c>: Gigabytes</li>
+    /// </ul>
+    /// </para>
+    /// </remarks>
+    public string? UnitFileSizeLimit { get; set; }
+
+    /// <summary>
+    /// Whether to throw an exception if an unsupported/invalid file is encountered. Defaults to <c>true</c>.
+    /// </summary>
+    public bool ThrowExceptionOnInvalidFile { get; set; } = true;
 }
--- src/ByteGuard.FileValidator.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/ByteGuard.FileValidator.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,12 +1,52 @@
+using ByteGuard.FileValidator.Configuration;
+using ByteGuard.FileValidator.Extensions.DependencyInjection.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace ByteGuard.FileValidator.Extensions.DependencyInjection;
 
+/// <summary>
+/// Extension methods for registering the File Validator services in an <see cref="IServiceCollection"/>.
+/// </summary>
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddFileValidator(this IServiceCollection services, Action<Configuration.FileValidatorSettingsConfiguration> configureSettings)
+    /// <summary>
+    /// Adds the File Validator services to the specified <see cref="IServiceCollection"/> with custom configuration options.
+    /// </summary>
+    /// <param name="services">Service collection.</param>
+    /// <param name="options">Configuration options.</param>
+    public static IServiceCollection AddFileValidator(this IServiceCollection services, Action<FileValidatorSettingsConfiguration> options)
     {
-        // TODO.
+        // Validate and setup configuration options.
+        services.AddSingleton<IValidateOptions<FileValidatorConfiguration>,
+            FileValidatorConfigurationOptionsValidator>();
+
+        services.Configure(options);
+
+        services.AddOptions<FileValidatorConfiguration>()
+            .Configure<IOptions<FileValidatorSettingsConfiguration>>((cfg, settings) =>
+            {
+                // Convert from FileValidatorSettingsConfiguration to FileValidatorConfiguration.
+                cfg.SupportedFileTypes = settings.Value.SupportedFileTypes;
+                cfg.ThrowExceptionOnInvalidFile = settings.Value.ThrowExceptionOnInvalidFile;
+
+                if (settings.Value.FileSizeLimit != -1)
+                {
+                    cfg.FileSizeLimit = settings.Value.FileSizeLimit;
+                }
+                else if (!string.IsNullOrWhiteSpace(settings.Value.UnitFileSizeLimit))
+                {
+                    cfg.FileSizeLimit = ByteSize.Parse(settings.Value.UnitFileSizeLimit);
+                }
+            })
+            .ValidateOnStart();
+
+        // Register the FileValidator service.
+        services.AddSingleton(serviceProvider =>
+        {
+            var configuration = serviceProvider.GetRequiredService<IOptions<FileValidatorConfiguration>>().Value;
+            return new FileValidator(configuration);
+        });
 
         return services;
     }
